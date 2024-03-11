c454265385a2bfcb607a11c7728753b99c53aae7
Temirlan Temirbaev <temirlan213@bk.ru>
2024-03-11 16:41:29 +0600

    high layers

--- cmd/main.go
+++ cmd/main.go
@@ -3,15 +3,28 @@ package main
 import (
 	"log"
 
+	"github.com/spf13/viper"
 	todo "learn-rest-api.go"
 	"learn-rest-api.go/pkg/handler"
+	"learn-rest-api.go/pkg/repository"
+	"learn-rest-api.go/pkg/service"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	if err := initConfig(); err != nil {
+		log.Fatalf("error while initializing config : %s ", err.Error())
+	}
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 	srv := new(todo.Server)
-	if err := srv.Run("3000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server, %s", err.Error())
 	}
+}
 
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
